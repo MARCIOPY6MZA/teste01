69d086f0b75b3fb9e20e38ca432270e998824a1b
MARCIOPY6MZA <py6mza@gmail.com>
2021-07-25 20:43:28 -0300

    segunda alteracao

--- projetoteste/src/projetoteste/ProcetoClassTeste.java
+++ projetoteste/src/projetoteste/ProcetoClassTeste.java
@@ -1,5 +1,18 @@
-package projetoteste;
+package projetoteste; 
 
 public class ProcetoClassTeste {
 
+	/* main é um metodo executavel em java*/
+	public static void main(String[] args) {
+		
+		/* escreve texto no console*/ 
+		System.out.println("Meu primeiro programa em java do curso formação web");
+		System.out.println("Java é orientado a objeto");
+		System.out.println("Java é orientado a objeto");
+		System.out.println("Java é orientado a objeto");	
+		System.out.println(" a oo");	
+		System.out.println(" obj");	
+		System.out.println("Java é orientado a objeto");	
+		
+	}
 }
